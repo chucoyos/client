18ae337b723c52255cf224b9f5e30fbcf457e3de
chucoyos <facturasmanzanillo@hotmail.com>
2021-12-10 13:19:24 -0600

    OAuth google sign-in

---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const StreamEdit = () => {
+  return (
+    <div>
+      <h2>StreamEdit</h2>
+      <Link className="ui button primary" to="/">Back to Streams</Link>
+    </div>
+  );
+};
+
+export default StreamEdit
