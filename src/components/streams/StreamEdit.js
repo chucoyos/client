ea0ed7087981671a82c5d4d80f5237fc0e99a95c
chucoyos <facturasmanzanillo@hotmail.com>
2022-01-17 00:47:52 -0600

    add StreamForm component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,13 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
 
-const StreamEdit = () => {
-  return (
-    <div>
-      <h2>StreamEdit</h2>
-      <Link className="ui button primary" to="/">Back to Streams</Link>
-    </div>
-  );
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <form className="ui form">
+          <input type="hidden" name="_method" value="put" />
+          <div className="field">
+            <label>Title</label>
+            <input
+              type="text"
+              name="title"
+              defaultValue={this.props.stream.title}
+            />
+          </div>
+          <div className="field">
+            <label>Description</label>
+            <textarea
+              name="description"
+              defaultValue={this.props.stream.description}
+            />
+          </div>
+          <button className="ui button primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, placeholder, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" placeholder={placeholder} />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+          className="ui error form"
+        >
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Enter Title"
+            type="text"
+            placeholder="Enter Title"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Enter Description"
+            type="text"
+            placeholder="Enter Description"
+          />
+          <button className="ui button primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamForm",
+  validate,
+})(StreamForm);
